174971709d714da0889d995f83bfea13c32279de
Abhi <learningcicd117@gmail.com>
2026-01-02 19:34:47 +0530

    update output message

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
 int main() {
-    std::cout << "Hello CI/CD you have achieved" << std::endl;
+    std::cout << "Hello CI/CD you have achieved best of luck abhishek" << std::endl;
     return 0;
 }
